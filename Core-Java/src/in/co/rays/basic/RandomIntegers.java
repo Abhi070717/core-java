117909bbf3816b057e77a84f5f8b8766e351d52d
Abhi070717 <abhibhawsar066@gmail.com>
2025-12-06 15:51:15 +0530

    core-java

--- Core-Java/src/in/co/rays/basic/RandomIntegers.java
+++
@@ -1,9 +0,0 @@
-package in.co.rays.basic;
-
-public class RandomIntegers {
-	public static void main(String[] args) {
-		for(int i = 0; i < 6; i++) {
-			System.out.println((int) (Math.random() * 200));
-		}
-	}
-}
--- Core-Java/src/in/co/rays/basic/TestString.java
+++
@@ -1,8 +0,0 @@
-package in.co.rays.basic;
-
-public class TestString {
-	public static void main(String[] args) {
-		String name = "Vijay Dinanath Chouhan";
-		System.out.println("String length - " + name.length());
-	}
-}
