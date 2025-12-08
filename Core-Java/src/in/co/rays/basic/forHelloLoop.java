b2209da9b119ecd36e9e23956693a36f3886c27e
Abhi070717 <abhibhawsar066@gmail.com>
2025-12-08 18:55:13 +0530

    core-java

--- Core-Java/src/in/co/rays/basic/forHelloLoop.java
+++ Core-Java/src/in/co/rays/basic/forHelloLoop.java
@@ -1,6 +1,6 @@
 package in.co.rays.basic;
 
-public class forHelloLoop {
+public class ForHelloLoop {
 	public static void main(String[]args) {
 		for(int i = 0; i < 5; i++) {
 			System.out.println("Hello Java");
