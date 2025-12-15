5d87427b332aea51c9075760d045458d830eee1e
Abhi070717 <you@example.comabhibhawsar066@gmail.com>
2025-12-15 18:29:34 +0530

    Done

---
+++ Java_OOPS/src/in/co/rays/oop/encaptulation/TestAccount.java
@@ -0,0 +1,37 @@
+package in.co.rays.oop.encaptulation;
+
+public class TestAccount {
+	public static void main(String[] args) {
+		Account Ram = new Account();
+
+		Ram.setnumber("95108423000021");
+		Ram.setAccountype("Saving");
+		Ram.setbalance(3454.76);
+
+		System.out.println("Ram's Account Number: " + Ram.getNumber());
+		System.out.println("Ram's Account Type: " + Ram.getAccountType());
+		System.out.println("Ram's Account Balance: " + Ram.getbalance());
+		Ram.Deposit(500);
+		Ram.withdrawal(400);
+		// Ram.fundTransfer(850);
+		Ram.payBill(349);
+
+		System.out.println("--------------------------------------");
+
+		Account Shayam = new Account();
+
+		Shayam.setnumber("95108423454523");
+		Shayam.setAccountype("Current");
+		Shayam.setbalance(30540.55);
+
+		System.out.println("Shayam's Account Number: " + Shayam.getNumber());
+		System.out.println("Shayam's Account Type: " + Shayam.getAccountType());
+		System.out.println("Shayam's Account Balance: " + Shayam.getbalance());
+		Shayam.Deposit(600);
+		Shayam.withdrawal(700);
+		Shayam.fundTransfer(Ram, 100);
+		Shayam.payBill(199);
+		Ram.fundTransfer(Shayam, 50);
+	}
+
+}
---
+++ Java_OOPS/src/in/co/rays/oop/encaptulation/TestPerson.java
@@ -0,0 +1,37 @@
+package in.co.rays.oop.encaptulation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class TestPerson {
+	public static void main(String[] args) throws ParseException {
+		Person R1 = new Person();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+		R1.setName("Ram Joshi");
+		R1.setDOB(sdf.parse("2002-07-17"));
+		R1.setAddress("Kukshi");
+
+		System.out.println("Customer Name: " + R1.getName());
+		System.out.println("Customer DOB: " + R1.getDOB());
+		System.out.println("Customer Address: " + R1.getAddress());
+		System.out.println("Customer Age: " + R1.getAge(R1.getDOB()));
+		
+		System.out.println("-------------------------------");
+		
+		Person S1 = new Person();
+		
+		S1.setName("Shayam gupta");
+		S1.setDOB(sdf.parse("2009-08-27"));
+		S1.setAddress("Manawar");
+		
+		System.out.println("Customer Name: " + S1.getName());
+		System.out.println("Customer DOB: " + S1.getDOB());
+		System.out.println("Customer Address: " + S1.getAddress());
+		System.out.println("Customer Age: " + S1.getAge(S1.getDOB()));
+		
+		
+	}
+
+}
